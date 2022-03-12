60b7352f3478665617738681046917d5557deff1
winson <winson.zhong@568win.com>
2022-03-12 22:16:25 +0800

    [Chapter 2] P12 Test  Get Red Light

--- KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
+++ KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
@@ -13,6 +13,8 @@ namespace KentBeckTDDSampleCodeTest
             Dollar five = new Dollar(5);
             five.Times(2);
             Assert.AreEqual(10, five.Amount);
+            five.Times(3);
+            Assert.AreEqual(15, five.Amount);
         }
     }
 }
