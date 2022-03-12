d81dcb7e96cc79311f800c79c8e9b9acb9f4b5ea
winson <winson.zhong@568win.com>
2022-03-12 21:42:43 +0800

    [Chapter 1] P4 Init TestMultiplication Test

---
+++ KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KentBeckTDDSampleCodeTest
+{
+    [TestClass]
+    public class MultiCurrencyTest
+    {
+        //5美元*2=10美元
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equals(10, five.amount);
+        }
+    }
+}
+}
