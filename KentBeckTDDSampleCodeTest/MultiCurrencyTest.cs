a3053817c08a41c5193f21eb031d30eeeff26fcd
winson <winson.zhong@568win.com>
2022-03-19 18:47:56 +0800

    [Chapter 6]  p32 add franc equal test by copy dollar

--- KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
+++ KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
@@ -28,6 +28,8 @@ namespace KentBeckTDDSampleCodeTest
         {
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
+            Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
         }
     }
 }
