1506372fb4acd72c6ffc6eb4f1f1046a929f85d6
winson <winson.zhong@568win.com>
2022-03-23 15:20:53 +0800

    [Chapter 7] P35 compare franc and dollar but return true is not expect

--- KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
+++ KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
@@ -30,6 +30,8 @@ namespace KentBeckTDDSampleCodeTest
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+            //compare franc and dollar will happen?
+            Assert.IsTrue(new Franc(5).Equals(new Dollar(5)));
         }
     }
 }
