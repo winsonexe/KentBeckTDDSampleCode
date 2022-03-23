8e4e594942737046d2839799201fcdb8067263fe
winson <winson.zhong@568win.com>
2022-03-23 15:45:51 +0800

    [Chapter 8] p41 use factory method(dollar) in test

--- KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
+++ KentBeckTDDSampleCodeTest/MultiCurrencyTest.cs
@@ -11,8 +11,8 @@ namespace KentBeckTDDSampleCodeTest
         public void TestDollarMultiplication()
         {
             Money five = Money.Dollar(5);
-            Assert.AreEqual(new Dollar(10), five.Times(2)); 
-            Assert.AreEqual(new Dollar(15), five.Times(3));  
+            Assert.AreEqual(Money.Dollar(10), five.Times(2)); 
+            Assert.AreEqual(Money.Dollar(15), five.Times(3));  
         }
 
         [TestMethod]
@@ -26,8 +26,8 @@ namespace KentBeckTDDSampleCodeTest
         [TestMethod]
         public void TestEquality()
         {
-            Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
-            Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
+            Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
             //compare franc and dollar will happen?
