2df7479140385a2c5eea1822c773a91d5175df14
Winson <s102213014@gmail.com>
2022-04-04 22:31:07 +0800

    [Chapter 10] revert to using factory

--- KentBeckTDDSampleCode/Dollar.cs
+++ KentBeckTDDSampleCode/Dollar.cs
@@ -9,7 +9,7 @@ namespace KentBeckTDDSampleCode
 
         public override Money Times(int multiplier)
         {
-            return Money.Dollar(Amount * multiplier);
+            return new Dollar(Amount * multiplier,"USD");
         }
 
     }
--- KentBeckTDDSampleCode/Franc.cs
+++ KentBeckTDDSampleCode/Franc.cs
@@ -8,7 +8,7 @@
 
         public override Money Times(int multiplier)
         {
-            return Money.Franc(Amount * multiplier);
+            return new Franc(Amount * multiplier,"CHF");
         }
     }
 }
