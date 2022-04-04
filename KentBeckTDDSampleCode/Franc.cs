8ab8191ccb7a3be40a00cf7f5b090c3e530701d1
Winson <s102213014@gmail.com>
2022-04-04 22:54:21 +0800

    [Chapter 10] p56 revert to  get green light

--- KentBeckTDDSampleCode/Franc.cs
+++ KentBeckTDDSampleCode/Franc.cs
@@ -8,7 +8,7 @@
 
         public override Money Times(int multiplier)
         {
-            return new Money(Amount * multiplier,currency);
+            return new Franc(Amount * multiplier,currency);
         }
     }
 }
